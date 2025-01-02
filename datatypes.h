42b6eb761c5212bbdcb79eda325a30665b32dea8
tienvuiet <tienxinhzai241@GMAIL.COM>
2025-01-02 23:18:06 +0700

    Add files via upload

---
+++ datatypes.h
@@ -0,0 +1,15 @@
+
+//kieu du lieu
+#include <stdbool.h>
+typedef struct {
+    int id;
+    char name[50];
+    float price;
+    int quantity;
+    int categoryId; 
+} Product;
+typedef struct {
+	int id;
+	char name[50];
+} Category;
+
